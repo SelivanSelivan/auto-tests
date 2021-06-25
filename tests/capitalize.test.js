4a1a2bce73102b77b85f26e6f55e26c646c03688
SelivanSelivan <Sash_2000_00@mail.ru>
2021-06-25 23:47:52 +0300

    2 lesson of auto tests

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,13 +1,9 @@
 #!/usr/bin/env node
 
+import {strict as assert} from 'assert';
 import { capitalize } from '../src/capitalize.js';
 
-if (capitalize('hello') !== 'Hello') {
-	  throw new Error('Функция работает неверно!');
-}
-
-if (capitalize('') !== '') {
-	  throw new Error('Функция работает неверно!');
-}
+assert.equal(capitalize('hello'), 'Hello');
+assert.equal(capitalize(''), '');
 
 console.log('Все тесты пройдены!');
